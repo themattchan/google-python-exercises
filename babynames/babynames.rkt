38a49eb516481c78761916c664e7f673e7d84e30
Matthew Chan <matt@themattchan.com>
2017-12-27 04:17:39 -0800

    it's been a while

--- babynames/babynames.rkt
+++ babynames/babynames.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
- (require (prefix-in h: html)
-          (prefix-in x: xml))
+(require (prefix-in h: html)
+         (prefix-in x: xml)
+         (prefix-in x: xml/path))
 
 ;; parsing xml with regex? are you nuts?
 ;; https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags
@@ -11,27 +12,29 @@
            h:read-html-as-xml
            open-input-file))
 
-;; gross
-(define (get-info lst)
-  (drop (car (drop
-              (car (drop
-                    (car (drop
-                          (car (drop
-                                (car (drop lst 2))
-                                5))
-                          2))
-                    2))
-              3))
-        3))
+(define (chunks-of n xs)
+  (if (< (length xs) n)
+      '()
+      (let-values ([(p s) (split-at xs n)])
+        (cons p (chunks-of n s)))))
 
-(define (bfs pat tree )
-  (match tree
-    [`(,pat ...) (car tree)]
-    ['()         (map (curry bfs pat) tree)]
-    [_           (bfs (cdr tree) pat)]))
+; output: ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+(define (count-names xs)  
+  (let* ((cleaned
+          (cons 'top
+                (x:se-path*/list '(html body table tbody tr td) (cons 'html xs))))
+         (date (string->number (last (string-split(x:se-path* '(top h3) cleaned)))))
+         (sort1 (λ (xs) (sort xs string<? #:key car)))
+         (names
+          (sort1
+           (apply append
+             (map (match-lambda [`(,n ,male, female)
+                                 (let ((n1 (string->number n)))
+                                   `((,male . ,n1) (,female . ,n1)))])
+               (chunks-of 3
+                 (filter string?
+                   (x:se-path*/list '(top table tbody tr td)
+                                    cleaned))))))))
+    (list date names)))
 
-(define (dfs tree pat) ...)
-
-
-;(define (extract-names file))
-(bfs `(h3 ...) (get-info (read-html-as-xexpr "baby1990.html"))
+(define (t) (count-names (read-html-as-xexpr "baby2006.html")))
